ced37eb4b128aa90fcee385c4c37159f56459d15
andrew <hi@andrew.im>
2023-05-23 22:21:53 +0900

    basic workflow to test

--- typescript/publish.sh
+++ typescript/publish.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 set -e
 
+# Check if NODE_AUTH_TOKEN is set
+if [[ -z "${NODE_AUTH_TOKEN}" ]]; then
+  echo "NODE_AUTH_TOKEN is not set. Please set the environment variable and try again."
+  exit 1
+fi
+
+# Navigate to the "packages" folder
+cd packages
+
+# Loop over each package in the "packages" folder
+for package in */; do
+  # Move into the package directory
+  cd "$package"
+
+  # Publish the package using yarn publish
+  yarn publish
+
+  # Move back to the parent directory
+  cd ..
+done
